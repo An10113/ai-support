7b85e21059d8893a015e8def56ba8b608c919770
An10113 <antranthanh1011@gmail.com>
2023-11-20 23:58:46 +0700

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,41 @@
-"use client"
+"use client";
+
+import { Menu,Sparkles } from "lucide-react";
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+
+const font = Poppins({
+    weight: "600",
+    subsets: ["latin"]
+})
 
 export const NavBar = () => {
-    <div className=" fixed w-full z-50 flex items-center py-2 px-4 justify-between">
-        Naviagtion Bar
+  return (
+    <div
+      className="fixed w-full z-50 flex 
+    items-center py-2 px-4 justify-between
+    border-b border-primary/10 bg-secondary
+    "
+    >
+      <div className="flex items-center">
+        <Menu className="block md:hidden" />
+        <Link href="/">
+        <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary", font.className)}>
+          Companion.AI
+        </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <Button variant={"premium"} size="sm">
+            Upgrade
+            <Sparkles className="h-4 w-4 fill-white text-white ml-2"/>
+        </Button>
+        <UserButton />
+      </div>
     </div>
-}
+  );
+};
