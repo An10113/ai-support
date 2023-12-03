5bf7557e717a7763fa47ef1eb955e0be9739665c
An10113 <antranthanh1011@gmail.com>
2023-12-03 16:25:32 +0700

    creation form

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -1,49 +1,93 @@
-"use client"
+"use client";
 
 import * as z from "zod";
 import { Category, Companion } from "@prisma/client";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
 
 const formSchema = z.object({
-    name: z.string().min(1, {
-        message : "Name is required."
-    }),
-    desciption: z.string().min(1, {
-        message : "desciption is required."
-    }),
-    instructions: z.string().min(200, {
-        message : "instructions required at least 200 characters"
-    }),
-    seed: z.string().min(200, {
-        message : "seed required at least 200 characters"
-    }),
-    src: z.string().min(1, {
-        message : "Image is required."
-    }),
-    categoryId: z.string().min(1, {
-        message : "Category is required."
-    }),
-})
+  name: z.string().min(1, {
+    message: "Name is required.",
+  }),
+  desciption: z.string().min(1, {
+    message: "desciption is required.",
+  }),
+  instructions: z.string().min(200, {
+    message: "instructions required at least 200 characters",
+  }),
+  seed: z.string().min(200, {
+    message: "seed required at least 200 characters",
+  }),
+  src: z.string().min(1, {
+    message: "Image is required.",
+  }),
+  categoryId: z.string().min(1, {
+    message: "Category is required.",
+  }),
+});
 
 interface CompanionFormProps {
-    initialData: Companion | null
-    categories : Category[]
+  initialData: Companion | null;
+  categories: Category[];
 }
 
-export const CompanionForm = ({ categories, initialData}: CompanionFormProps) => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: initialData || {
-            name: "",
-            desciption: "",
-            
-        }
-    })
-    return ( 
-        <>
-            Hello
-        </>
-     );
-}
- 
+export const CompanionForm = ({
+  categories,
+  initialData,
+}: CompanionFormProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: "",
+      desciption: "",
+      instructions: "",
+      seed: "",
+      src: "",
+      categoryId: undefined,
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="h-full p-4 space-y-2 max-w-3xl mx-auto">
+      <Form {...form}>
+        <form
+          className="space-y-8 pb-10"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <div className="space-x-2 w-full col-span-2">
+            <div>
+              <h3 className="text-lg font-medium">General Information</h3>
+              <p className="text-sm text-muted-foreground">
+                General Information about your Companion
+              </p>
+            </div>
+            <Separator className="bg-primary/10" />
+          </div>
+          <FormField
+            name="src"
+            render={({ field }) => (
+              <FormItem className="flex flex-col items-center justify-center space-y-4">
+                <FormControl>Image Upload content</FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField>
+        </form>
+      </Form>
+    </div>
+  );
+};
