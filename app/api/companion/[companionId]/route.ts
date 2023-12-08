221b94b70985ff69d62d5072c5dd42439748a0a3
An10113 <antranthanh1011@gmail.com>
2023-12-08 21:18:00 +0700

    companion API

---
+++ app/api/companion/[companionId]/route.ts
@@ -0,0 +1,36 @@
+import { auth, currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+
+export async function PATCH( req:Request,{params}:{params: {companionId:string}}) {
+    try{
+        const body = await req.json();
+        const user = await currentUser();
+        const { src, name, description, instructions, seed, categoryId } = body;
+        
+        if(params.companionId){
+            return new NextResponse("CompanionID is required",{status : 400})
+        }
+
+        if (!user || !user.id || !user.firstName) {
+          return new NextResponse("Unauthorized", { status: 401 });
+        }
+    
+        if (!src || !name || !description || !instructions || !seed || !categoryId) {
+          return new NextResponse("Missing required fields", { status: 400 });
+        };
+
+        const companion = await prismadb.companion.create({
+            data:{
+                src, name, description, seed, instructions, categoryId, userId: user.id, userName: user.firstName,  
+            }
+        })
+
+        return NextResponse.json(companion)
+    }
+    catch (error){
+        console.log("[COMPANION_PATCH]", error);
+        return new NextResponse("Internal Error", {status: 500})
+    }
+}
