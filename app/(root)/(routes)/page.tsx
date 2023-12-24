acb7f5fd72df42ccf302cf2403649cd0007b4f5d
An10113 <antranthanh1011@gmail.com>
2023-12-24 15:41:05 +0700

    aaa

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,29 +1,47 @@
-
 import { Categories } from "@/components/Categories";
+import Companion from "@/components/compainion";
 import SearchInput from "@/components/search-input";
-import prismadb from "@/lib/prismadb";
-import { UserButton } from "@clerk/nextjs";
+import prismadb from "@/lib/prismadb"
+
 
 interface RootPageProps {
-    searchParams: {
-        categoryId: string;
-        name: String;
-    }
-}
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+};
 
+const RootPage = async ({
+  searchParams
+}: RootPageProps) => {
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+    include: {
+      _count: {
+        select: {
+          message: true,
+        }
+      }
+    },
+  });
 
-const RootPage = async ({searchParams} : RootPageProps) => {
+  const categories = await prismadb.category.findMany();
 
-    
-    const category = await prismadb.category.findMany()
-
-    return ( 
+  return (
     <div className="h-full p-4 space-y-2">
-        <SearchInput />
-        <Categories data={category}/>
-    </div> 
-    );
+      <SearchInput />
+      <Categories data={categories} />
+      <Companion data={data} />
+    </div>
+  )
 }
- 
-export default RootPage;
 
+export default RootPage
